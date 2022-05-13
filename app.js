c66c8e1813cefb2320232e96562ec41ce483adfc
yufanme <562937707@qq.com>
2022-05-13 22:19:02 +0800

    add git ignore and update ports

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://Fan:mongodb009527@cluster0.pwed7.mongodb.net/todoDB");
 
 const itemsSchema = new mongoose.Schema({
-  name: String,
+  name: String, 
 });
 const Item = mongoose.model("Item", itemsSchema);
 
@@ -131,6 +131,12 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
 });
+
